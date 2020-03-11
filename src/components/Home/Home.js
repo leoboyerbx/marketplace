00b0927c64077634f00bf25ace3bac967d8f9b4d
leoboyerbx <leo.boyer@outlook.com>
2020-03-11 17:32:50 +0100

    Added firidenly hom emessage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,7 @@ import { withAuth } from '../Authentication'
 export function Home(props) {
     
     return (
-        <h1>{props.isLogged ? "Bienvenue" : "Veuillez vous logger"}</h1>
+        <h1>{props.auth.loggedUser ? props.auth.loggedUser.displayName ? "Bienvenue " + props.auth.loggedUser.displayName : '' : "Veuillez vous logger"}</h1>
     )
 }
 export default withAuth(Home)
